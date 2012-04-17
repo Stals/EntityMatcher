6e25af03be9c59fba4982caa94bcbabefaa39a4a
Stanislav Korotaev <superstals@gmail.com>
2012-04-18 01:40:11 +0400

    added matching algorithm discription and also match method

--- entitymatcher.h
+++ entitymatcher.h
@@ -1,5 +1,16 @@
 /* This is a library for matching two entities and buildng a list of entities sorted by rating (flickchart.com style). 
 You can match any Entites. For example it could be games, films, books, board games and so on.
+This library stores list of entities id's and also moves entities depending on a user choice between them.
+
+Mathcing algorithm:
+	Both entities are on the list:
+		If an entity that was higher won - nothing changed
+		Else - winner goes above the looser
+	Only one entity is on the list:
+		If an entity that wasn't in a list won - it goes above the looser
+		If an entity that was in a list won - looser goes to the end of a list (push_back(looser))
+	Neither of entities are on the list:
+		Winner goes above the looser, both entities added in the end of a list (push_back(winner), push_back(looser))
 */
 
 #include <string>
@@ -15,6 +26,9 @@ public:
 	const std::vector<int>& getEntities(){
 		return entities;
 	}
+
+	// mathes 2 entities changing their rating. Variable choice could equals to 1 or 2 meaning wich entity won (entity with id1 or id2).
+	void match(int id1, int id2, int choice);
 
 private:
 	// Todo rewrite as a list and compare speed
